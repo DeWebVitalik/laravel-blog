a18c263f260936dd54c2b23dd703ae9bcd7fb71b
Vitaliy Kondratyuk <vitalik.deweloper@gmail.com>
2020-01-09 00:47:13 +0200

    Add fkey and index in tables

---
+++ database/migrations/2020_01_08_215519_add_fkey_and_pkey.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFkeyAndPkey extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('post', function (Blueprint $table) {
+            $table->bigInteger('category_id')->unsigned();
+            $table->foreign('category_id')->references('id')->on('category');
+            $table->index(['category_id']);
+        });
+
+        Schema::table('post_comment', function (Blueprint $table) {
+            $table->bigInteger('post_id')->unsigned();
+            $table->foreign('post_id')->references('id')->on('post');
+            $table->index(['post_id']);
+        });
+
+        Schema::table('category_comment', function (Blueprint $table) {
+            $table->bigInteger('category_id')->unsigned();
+            $table->foreign('category_id')->references('id')->on('category');
+            $table->index(['category_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
