6f21d3c999b48c79c40dfccf344712435cc25f60
Vitaliy Kondratyuk <vitalik.deweloper@gmail.com>
2020-01-08 23:07:40 +0200

    Add post table

---
+++ database/migrations/2020_01_08_205150_create_post_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 200)->nullable(false);
+            $table->text('content')->nullable(false);
+            $table->string('file', 200);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post');
+    }
+}
