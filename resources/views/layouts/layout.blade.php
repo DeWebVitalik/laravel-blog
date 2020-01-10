7efbdc67d38dd96141521bd8b52f65ec37735022
Vitaliy Kondratyuk <vitalik.deweloper@gmail.com>
2020-01-10 21:50:00 +0200

    Add npm and include bootstrap

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,83 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
-
+    <link rel="icon" href="/favicon.ico">
     <title>Blog Template for Bootstrap</title>
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/blog/">
-
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-    <!-- Custom styles for this template -->
-    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
-    <link href="/css/main.css" rel="stylesheet">
+    <link href="{{URL::asset('css/app.css')}}" rel="stylesheet">
 </head>
-
 <body>
-
 <div class="container">
     @include('layouts.header')
     @include('layouts.nav')
-{{--    <div class="row mb-2">--}}
-{{--        <div class="col-md-6">--}}
-{{--            <div class="card flex-md-row mb-4 box-shadow h-md-250">--}}
-{{--                <div class="card-body d-flex flex-column align-items-start">--}}
-{{--                    <strong class="d-inline-block mb-2 text-primary">World</strong>--}}
-{{--                    <h3 class="mb-0">--}}
-{{--                        <a class="text-dark" href="#">Featured post</a>--}}
-{{--                    </h3>--}}
-{{--                    <div class="mb-1 text-muted">Nov 12</div>--}}
-{{--                    <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to--}}
-{{--                        additional content.</p>--}}
-{{--                    <a href="#">Continue reading</a>--}}
-{{--                </div>--}}
-{{--                <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb"--}}
-{{--                     alt="Card image cap">--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--        <div class="col-md-6">--}}
-{{--            <div class="card flex-md-row mb-4 box-shadow h-md-250">--}}
-{{--                <div class="card-body d-flex flex-column align-items-start">--}}
-{{--                    <strong class="d-inline-block mb-2 text-success">Design</strong>--}}
-{{--                    <h3 class="mb-0">--}}
-{{--                        <a class="text-dark" href="#">Post title</a>--}}
-{{--                    </h3>--}}
-{{--                    <div class="mb-1 text-muted">Nov 11</div>--}}
-{{--                    <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to--}}
-{{--                        additional content.</p>--}}
-{{--                    <a href="#">Continue reading</a>--}}
-{{--                </div>--}}
-{{--                <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb"--}}
-{{--                     alt="Card image cap">--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
 </div>
 <main role="main" class="container">
     @include('flash::message')
     @yield('content')
 </main><!-- /.container -->
 @include('layouts.footer')
-
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-        crossorigin="anonymous"></script>
-<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-<script src="../../assets/js/vendor/popper.min.js"></script>
-<script src="../../dist/js/bootstrap.min.js"></script>
-<script src="../../assets/js/vendor/holder.min.js"></script>
-<script>
-    Holder.addTheme('thumb', {
-        bg: '#55595c',
-        fg: '#eceeef',
-        text: 'Thumbnail'
-    });
-</script>
+<script src="{{URL::asset('js/app.js')}}"></script>
 </body>
 </html>
